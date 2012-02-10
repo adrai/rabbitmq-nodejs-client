5946c54cff1303552a5d3cd7070c517d3ac00862
Adriano Raiano <adriano.raiano@kaba.com>
2012-02-10 15:05:57 +0100

    add example

---
+++ example/pub.js
@@ -0,0 +1,9 @@
+var rabbitHub = require('../lib/hub');
+
+var pubHub = rabbitHub.create( { task: 'pub', channel: 'myChannel' } );
+pubHub.on('connection', function(hub) {
+
+    hub.send('Hello World!');
+
+});
+pubHub.connect();
---
+++ example/sub.js
@@ -0,0 +1,11 @@
+var rabbitHub = require('../lib/hub');
+
+var subHub = rabbitHub.create( { task: 'sub', channel: 'myChannel' } );
+subHub.on('connection', function(hub) {
+
+    hub.on('message', function(msg) {
+        console.log(msg);
+    }.bind(this));
+
+});
+subHub.connect();
